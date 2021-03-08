76bdd7eaafc6e076bc37345b309b20fa49f7ee12
cappuccino <scaft91008@gmail.com>
2021-03-08 19:33:20 +0800

    add login_request

--- todolist/views.py
+++ todolist/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import login, logout, authenticate
 from .form import TodoForm
 from .models import Todo
 from django.utils import timezone
-
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'todo/home.html')
@@ -33,7 +33,7 @@ def signupuser(request):
                           {'form': UserCreationForm(), 'error': 'Password did not match'})
             # Tell the user the passwords didn't match
 
-
+@login_required()
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
@@ -51,7 +51,7 @@ def loginuser(request):
             login(request, user)
             return redirect('currenttodos')
 
-
+@login_required()
 def createtodo(request):
     if request.method == 'GET':
         return render(request, 'todo/createtodo.html', {'form': TodoForm()})
@@ -65,7 +65,7 @@ def createtodo(request):
         except:
             return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error': 'Bad data, Try again.'})
 
-
+@login_required()
 def currenttodos(request):
     # 只顯示當前使用者的todo
     todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
@@ -73,12 +73,13 @@ def currenttodos(request):
 
     return render(request, 'todo/currenttodos.html', {'todos': todos})
 
+@login_required()
 def completedtodos(request):
     todos = Todo.objects.filter(user=request.user,  datecompleted__isnull=False).order_by('-datecompleted')
 
     return render(request, 'todo/completedtodos.html', {'todos': todos})
 
-
+@login_required()
 def viewtodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == 'GET':
@@ -92,7 +93,7 @@ def viewtodo(request, todo_pk):
         except ValueError:
             return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form, 'error': "bad info"})
 
-
+@login_required()
 def completetodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == 'POST':
@@ -100,7 +101,7 @@ def completetodo(request, todo_pk):
         todo.save()
         return redirect('currenttodos')
 
-
+@login_required()
 def deletetodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == 'POST':
