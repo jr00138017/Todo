b854ea2fb8321c19d13f0e6e84eab4347ca0a3a2
Ansel <scaft91008@gmail.com>
2021-03-23 19:04:55 +0800

    finish CRUD API

--- api/serializers.py
+++ api/serializers.py
@@ -6,7 +6,13 @@ class TodolistSerializer(serializers.ModelSerializer):
     created = serializers.ReadOnlyField()
     datecompleted = serializers.ReadOnlyField()
 
-
     class Meta:
         model = Todo
         fields = ['id', 'title', 'memo', 'created', 'datecompleted', 'important']
+
+
+class TodoCompleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        read_only_fields = ['title', 'memo', 'created', 'datecompleted', 'important']
+        fields = ['id']
--- api/views.py
+++ api/views.py
@@ -1,12 +1,46 @@
+from django.utils import timezone
 from rest_framework import generics, permissions
-from .serializers import TodolistSerializer
+from .serializers import TodolistSerializer , TodoCompleteSerializer
 from todolist.models import Todo
 
 
-class TodoCompleted(generics.ListAPIView):
+class TodoCompletedList(generics.ListAPIView):
     serializer_class = TodolistSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
         return Todo.objects.filter(user=user, datecompleted__isnull=False).order_by('-datecompleted')
+
+
+class TodoListCreate(generics.ListCreateAPIView):
+    serializer_class = TodolistSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user, datecompleted__isnull=True)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodolistSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
+
+class TodoComplete(generics.UpdateAPIView):
+    serializer_class = TodoCompleteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
+
+    def perform_update(self, serializer):
+        serializer.instance.datecompleted = timezone.now()
+        serializer.save()
